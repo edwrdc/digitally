c2abb34437291065202d7c43a0e8d01a28fe7041
Edward <edward.chan8026@gmail.com>
2024-12-05 19:37:48 +0800

    feat: add jsonResponse method

--- cmd/api/errors.go
+++ cmd/api/errors.go
@@ -21,3 +21,11 @@ func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request)
 	log.Printf("Not found: %s, path:%s", r.Method, r.URL.Path)
 	writeJSONError(w, http.StatusNotFound, "not found")
 }
+
+func (app *application) jsonResponse(w http.ResponseWriter, status int, data any) {
+	type envelope struct {
+		Data any `json:"data"`
+	}
+
+	writeJSON(w, status, &envelope{Data: data})
+}
