58569c42fa31769aefa5810dc77b20e73a09c726
Edward <edward.chan8026@gmail.com>
2024-12-05 17:02:20 +0800

    feat: add error handling responses for server, bad request, and not found status.

---
+++ cmd/api/errors.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func (app *application) serverErrorResponse(w http.ResponseWriter, r *http.Request, err error) {
+	// TODO: Introduce structured logging later
+	log.Printf("Internal server error: %s, path:%s, error: %s", r.Method, r.URL.Path, err)
+
+	writeJSONError(w, http.StatusInternalServerError, "encountered an error while processing the request")
+}
+
+func (app *application) badRequestResponse(w http.ResponseWriter, r *http.Request, err error) {
+	log.Printf("Bad request: %s, path:%s, error: %s", r.Method, r.URL.Path, err)
+	writeJSONError(w, http.StatusBadRequest, err.Error())
+}
+
+func (app *application) notFoundResponse(w http.ResponseWriter, r *http.Request) {
+	log.Printf("Not found: %s, path:%s", r.Method, r.URL.Path)
+	writeJSONError(w, http.StatusNotFound, "not found")
+}
